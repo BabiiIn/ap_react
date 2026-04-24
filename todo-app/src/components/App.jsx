a1f61c68d6dfed700905044315479ed9c0193270
Ins <2518949@gmail.com>
2026-04-24 16:06:56 +0100

    feat: add todo form with input validation and UUID ids

--- todo-app/src/components/App.jsx
+++ todo-app/src/components/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { TodoList } from './TodoList';
+import { v4 as uuidv4 } from 'uuid';
 import styles from './App.module.css';
+import { useRef } from 'react';
 
 const initial = [
   { id: 1, text: 'Выучить React', completed: false },
@@ -9,12 +11,45 @@ const initial = [
 ];
 
 export function App() {
-  const [todos, setTodo] = useState(initial);
+  // const [nextId, setNextId] = useState(4);
+  const [todos, setTodos] = useState(initial);
+  const [inputValue, setInputValue] = useState('');
+  const [error, setError] = useState('');
+  const inputRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (!inputValue.trim() || inputValue.length < 2) {
+      setError('Введите хотя бы 2 символа');
+      setInputValue('');
+      return;
+    }
+
+    setTodos([...todos, { id: uuidv4(), text: inputValue, completed: false }]);
+    inputRef.current.focus();
+    setError('');
+    setInputValue('');
+    // setNextId((prev) => prev + 1);
+  };
 
   return (
     <div className={styles.app}>
       <h1 className={styles.title}>What to do</h1>
       <TodoList todos={todos} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="todo-input">Введите новую задачу</label>
+        <input
+          ref={inputRef}
+          id="todo-input"
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        ></input>
+
+        <button>Добавить</button>
+        <div>{error && <p className={styles.error}>{error}</p>}</div>
+      </form>
     </div>
   );
 }
